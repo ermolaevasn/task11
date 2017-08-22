b2fdbabcc44c9d21ef5754e14e0f14d211108ad8
Дом <@DESKTOP-7GF1H2I>
2017-08-23 00:38:01 +0500

    готово +коментарии и расшифровка

--- задание11/Program.cs
+++ задание11/Program.cs
@@ -10,39 +10,78 @@ namespace задание11
     {
         static void Main(string[] args)
         {
-            int n;
-            Console.WriteLine(@"Данная программа шифрует выражения
-Введите выражение, состоящее из 0 и 1");
-            string s = Console.ReadLine();
+            int n,cases;
+            bool ok=false;
+            Console.WriteLine("Введите выражение, состоящее из 0 и 1");
+            string s = Console.ReadLine();//ввод шифруемого выражения
             Proverka("выражение",ref s);
-            char[] array = s.ToCharArray();
-            int[] a = new int[array.Length];
-            int[] b = new int[array.Length];
-            n = array.Length;
+            char[] array = s.ToCharArray();//представление выр-ия в виде массива
+            int[] a = new int[array.Length];//массив для выражения
+            int[] b = new int[array.Length];//массив для зашифрованного выражения
+            int[] c = new int[array.Length];//массив для расшифрованного выражения
+            n = array.Length;//длина выражения
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++)//перевод выражения в массив
             {
                 a[i]= Convert.ToInt16(array[i])- 48;
-                Console.Write(a[i] + " ");
             }
             Console.WriteLine();
+
             b[0] = a[0];
 
-            for(int i=1;i<n;i++)
+            do
             {
-                if (a[i - 1] == a[i]) b[i] = 1;
-                else b[i] = 0;
-            }
+                Console.WriteLine(@"1. Зашифровать выражение
+2. Расшифровать выражение");
 
-            Console.WriteLine("Закодировано:");
-            for (int i = 0; i < n; i++) Console.Write(b[i] + " ");
+                Vvod("действие", out cases);
+                switch (cases)
+                {
+                    case 1:
+                        for (int i = 1; i < n; i++)//шифрование выражения
+                        {
+                            if (a[i - 1] == a[i]) b[i] = 1;
+                            else b[i] = 0;
+                        }
+                        Console.WriteLine("Результат зашифровки:");
+                        for (int i = 0; i < n; i++) Console.Write(a[i] + " ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("-> ");
+                        Console.ResetColor();
+                        for (int i = 0; i < n; i++) Console.Write(b[i] + " ");
+                        ok = true;
+                        break;
+                    case 2://расшифрование выражения
+                        c[0] = a[0];
+                        for (int i = 1; i < n; i++)
+                        {
+                            if (a[i] == 0)
+                            {
+                                if (c[i - 1] == 0) c[i] = 1;
+                                else c[i] = 0;
+                            }
+                            else c[i] = a[i - 1];
+                        }
+                        Console.WriteLine("Результат расшифровки:");
+                        for (int i = 0; i < n; i++) Console.Write(a[i] + " ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("-> ");
+                        Console.ResetColor();
+                        for (int i = 0; i < n; i++) Console.Write(c[i] + " ");
+                        ok = true;
+                        break;
+                    default:
+                        Console.WriteLine("Введите один из предложенных вариантов");
+                        ok = false;
+                        break;
+                }
+            } while (ok==false);
             Console.ReadKey();
         }
 
-        static void Proverka(string s, ref string a)//проверка ввода
+        static void Proverka(string s, ref string a)//проверка ввода выражения
         {
             bool ok = false;
-            string buf;
             do
             {
                 int kol = 0;
@@ -60,6 +99,19 @@ namespace задание11
                     ok = false;
                 }
             } while (!ok);
+        }
+        static double Vvod(string s, out int n)//ввод чисел с проверкой
+        {
+            bool ok;
+            string buf;
+            do
+            {
+                Console.Write(s + " = ");
+                buf = Console.ReadLine();
+                ok = Int32.TryParse(buf, out n);
+                if (!ok) Console.WriteLine("Введите " + s + " заново");
+            } while (!ok);
+            return n;
         }
     }
 }
